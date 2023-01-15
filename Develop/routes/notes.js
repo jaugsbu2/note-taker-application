8cd9525cf59e81634874b99e133504677e4d68fd
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-01-15 13:09:24 -0700

    remove id generator dependency, add note delete route

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,39 +1,46 @@
 const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const {readAndAppend, readFromFile} = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-// notes.get('/', (req, res) =>
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-// );
-
 notes.post('/', (req, res) => {
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('Error in posting note');
-    }
-  });
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-  module.exports = notes
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+notes.delete('/:id', (req, res) => {
+  const noteid = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteid);
+      writeToFile('./db/db.json', result);
+      res.json(`Note ${noteid} has been deleted`);
+    });
+});
+
+module.exports = notes
